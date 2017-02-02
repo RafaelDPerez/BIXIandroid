097a2002439c276bcc8038ed39e179bca39aec07
Johnny Gil Mejia <JohnnyGilMejia@gmail.com>
2017-02-01 20:50:59 -0400

    newMethod/Injector.java/provideLoggingHttpClient

--- BIXI/app/src/main/java/com/bixi/bixi/Network/Injector.java
+++ BIXI/app/src/main/java/com/bixi/bixi/Network/Injector.java
@@ -1,23 +1,25 @@
 package com.bixi.bixi.Network;
 
+import com.bixi.bixi.BuildConfig;
 import com.bixi.bixi.Interfaces.LoginService;
 import com.bixi.bixi.Utility.Constants;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by telynet on 1/21/2017.
+ * Created by JohnnyGilMejia on 1/21/2017.
  */
 
 public class Injector {
 
 
 
-    public static Retrofit provideRetrofit2(String baseUrl)
+    private static Retrofit provideRetrofit2(String baseUrl)
     {
 
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
@@ -26,16 +28,26 @@ public class Injector {
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create(provideGsonSetLetient()))
-                .client(httpClient.build())
+                .client(provideLoggingCapableHttpClient())
                 .build();
     }
 
-    public static Gson provideGsonSetLetient()
+    private static Gson provideGsonSetLetient()
     {
 
         return new GsonBuilder()
             .setLenient()
             .create();
+    }
+
+    private static OkHttpClient provideLoggingCapableHttpClient()
+    {
+        HttpLoggingInterceptor loggin = new HttpLoggingInterceptor();
+        loggin.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+
+        return new OkHttpClient.Builder()
+                .addInterceptor(loggin)
+                .build();
     }
 
     public  static UserService provideCreateUserService()
